0d8cd7a145cc535da3f61e67cdd3b407ac507980
Paul\0 <paul-marie.bettinelli@epitech.eu>
2020-03-03 21:17:50 -0500

    add `cron.js`
    This file need to be executed each day at 0h00 to send almanax to everybody;

---
+++ src/cron.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const Utils = require('../src/utils.js');
+const moments = require('moment-timezone');
+const client = new Discord.Client();
+const fs = require('fs');
+
+client.login("NjQyOTM1NDYzMDQ4NjQyNTcw.Xchc6g.Rp4_cHb9aXFBf4C_MIrPyZJBuqA").then(async () => {
+    const guilds = [];
+    const channels = [];
+    const date = moments().tz("Europe/Paris");
+    const almanax = Utils.getDate(date.format("DD/MM"))[0];
+    const embed = Utils.createEmbed(almanax);
+    await fs.readFile("./resources/auto_channel_id", "utf-8", async (err, data) => {
+        const buff = data.split("\n");
+        for (const line of buff) {
+            const tmp = line.split(": ");
+            if (tmp.length === 2) {
+                guilds.push(tmp[0]);
+                channels.push(tmp[1]);
+            }
+        }
+        await client.guilds.map(guild => {
+            if (guilds.includes(guild.id))
+                guild.channels.map(channel => {
+                    if (channels.includes(channel.id))
+                        client.channels.get(channel.id).send(embed);
+                });
+        });
+    });
+});
+
+setTimeout(() => { client.destroy() }, 60000);
+
