58bb81690efe951f2be517d0540297ba8b65c6ee
Lethargi <paul-marie.bettinelli@epitech.eu>
2020-07-15 19:14:36 +0200

    remove discord's token from code
    - Put discord's token into `./resources/config.json` instead of hardcoding it.
    - Add a mere script to populate the database before to use Bruno for the first time.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as mongoose from 'mongoose';
+import * as config from "../resources/config.json";
 import bot from "./discord";
 
-const databaseURL = process.env.MONGODB_URI || 'mongodb://localhost/ugram';
+const databaseURL = process.env.MONGODB_URI || 'mongodb://localhost/bruno';
 
 async function run() {
     try {
@@ -13,9 +14,14 @@ async function run() {
         console.error(err);
         process.exit(1);
     }
+
     console.log(`Connected to database at ${databaseURL}`);
     try {
-        bot.login("NjQyOTM1NDYzMDQ4NjQyNTcw.Xchc6g.Rp4_cHb9aXFBf4C_MIrPyZJBuqA");
+        if (process.argv.length === 3) {
+            let script = await import(`${process.argv[2]}`)
+            script.default();
+        } else
+            bot.login(config.discord.token);
     } catch (err) {
         console.error(err)
         process.exit(1);
--- src/models/server.ts
+++ src/models/server.ts
@@ -1,13 +1,13 @@
 import * as mongoose from 'mongoose';
 
 const serverSchema = new mongoose.Schema({
-    identifier: { type: Number, required: true, unique: true },
+    identifier: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     lang: { type: Number, required: true, default: 0 },
     server_id: { type: Number, required: true, default: 2 },
-    auto_mode: { type: Boolean, required: true, default: false },
     prefix: { type: String, required: true, default: "!bruno " },
-    default_channel: { type: String },
+    auto_mode: { type: Boolean, required: true, default: false },
+    auto_channel: { type: String },
 });
 
 export default mongoose.model("Server", serverSchema);
