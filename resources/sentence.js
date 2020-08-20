e1e0a01b1eccf37de8f544c87395f2264fb44e13
Lethargi <paul-marie.bettinelli@epitech.eu>
2020-08-20 23:26:39 +0200

    Restructure `list` embed with colored emoji

--- resources/sentence.js
+++ resources/sentence.js
@@ -128,10 +128,38 @@ const type_message = {
     "Quete": ['Expérience des quêtes', 'Quête répétable', 'Quêtes et kamas'],
 }
 
+const list_message = {
+    xp: {
+        Butin: ['Butin', 'Butin et XP dans la Maison Fantôme', 'Butin frigostien', 'Butin et XP sur tous les Bouftous', 'Butin et XP sur les pirates', 'Butin et XP sur les créatures marines', 'Butin et XP en slip'],
+        Challenge: ['Challenge supplémentaire', 'Challenges augmentés'],
+        Etoile: ['Apparition des étoiles', 'Étoiles défilantes'],
+        "Point d'Experience": ["Points d'expérience", 'Expérience du Kolizéum', 'Expérience des quêtes', 'Butin et XP dans la Maison Fantôme', 'Butin et XP sur tous les Bouftous', 'Butin et XP sur les pirates', 'Butin et XP sur les créatures marines', 'Butin et XP en slip'],
+        Quete: ['Expérience des quêtes', 'Quête répétable', 'Quêtes et kamas']
+    },
+    job: {
+        Alchimiste: ["Ceuillette abondante"],
+        Bucheron: ['Bois abondant'],
+        Chasseur: ['Gibier abondant'],
+        Craft: ['Objets de qualité', "Économie d'ingrédients", 'Fabrication intensive', 'Fabrique Féérique', 'Bonta et Brâkmar'],
+        "Economie d'Ingredients": ["Économie d'ingrédients"],
+        Metier: ['Apparition des ressources', 'Apparition des ressources et des Archimonstres', 'Fabrication intensive', 'Fabrique Féérique', 'Objets de qualité', "Économie d'ingrédients", 'Bonta et Brâkmar'],
+        Mineur: ['Minerai abondant'],
+        Paysan: ['Récolte abondante'],
+        Pecheur: ['Pêche abondante']
+    },
+    miscellaneous: {
+        Buff: ['Bénédiction de Miss Triste', 'Bénédiction du Fin Patraque', 'Vitalité débordante'],
+        Elevage: ['Élevage de Dragodindes'],
+        Familier: ['Familiers Frénétiques'],
+        Percepteur: ['Percepteurs avides', 'Percepteurs zélés']
+    }
+}
+
 module.exports = {
     empty_message,
     failure_message,
     type_message,
     help_message,
-    zodiac_list
+    zodiac_list,
+    list_message
 }
--- src/commands/list.ts
+++ src/commands/list.ts
@@ -3,11 +3,18 @@ import { Message, RichEmbed } from 'discord.js';
 
 // TODO to comment
 export const list = (message: Message) => {
-    let type_list: string = "";
-    for (const title of Object.keys(sentences.type_message))
-        type_list += `▫️ ${title}\n`;
+    let type_list: Array<string> = ['', '', ''];
+    for (const title of Object.keys(sentences.list_message.xp))
+        type_list[0] += `🔹 ${title}\n`;
+    for (const title of Object.keys(sentences.list_message.job))
+        type_list[1] += `▫️ ${title}\n`;
+    for (const title of Object.keys(sentences.list_message.miscellaneous))
+        type_list[2] += `🔸 ${title}\n`;
     const embed: RichEmbed  = new RichEmbed()
         .setColor("0x4E4EC8")
-        .addField("Liste des Bonus Almanax:", type_list)
+        .setTitle("Liste des Bonus Almanax:")
+        .addField("XP / Drop:", type_list[0])
+        .addField("Métier:", type_list[1])
+        .addField("Divers", type_list[2])
     message.channel.send({ embed });
 }
