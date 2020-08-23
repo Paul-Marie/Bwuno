f501b5756c0d0bae9302618804994aaffca348b9
Lethargi <paul-marie.bettinelli@epitech.eu>
2020-08-23 14:56:29 +0200

    add almanax's item search to `almanax` command
    You can now search almanax's dates from an item with `almanax` command,
    like:
    `${prefix}almanax laine du boufcoul`
    instead of `${prefix}item laine du boufcoul`

--- src/commands/almanax.ts
+++ src/commands/almanax.ts
@@ -1,5 +1,5 @@
 import * as sentences from "../../resources/sentence";
-import { getDate, formatDate, createEmbed, createFutureEmbed } from "../../src/utils";
+import { getDate, formatDate, getList, createEmbed, createFutureEmbed } from "../../src/utils";
 import { Message, MessageEmbed } from 'discord.js';
 
 export const almanax = async (message: Message, line: string[], config: any): Promise<Message> => {
@@ -15,11 +15,28 @@ export const almanax = async (message: Message, line: string[], config: any): Pr
                 const required_almanax: number = Number(param.slice(1, param.length));
                 const embed: MessageEmbed = createFutureEmbed(required_almanax);
                 message.channel.send(embed);
-            } else
-                message.channel.send("Je n'ai pas compris cette date.")
+            } else {
+                line.unshift(config.prefix)
+                item(message, line, config);
+            }
         } else {
             const embed: MessageEmbed = await createEmbed(almanax, config.server);
             message.channel.send(embed);
         }
+    }
+}
+
+//
+export const item = async (message: Message, line: string[], config: any): Promise<Message> => {
+    if (line.length === 1)
+        return message.channel.send("Tu as oublié l'item.");
+    line.shift();
+    const argument: string = line.join(' ').toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    const result: any[] = getList(argument);
+    if (!result[0])
+        return message.channel.send("Malgré mes recherches, je n'ai pas trouvé cet item dans la liste des offrandes... Peut etre l'as tu mal orthographié? (Vérifie l'orthographe sur l'encyclopédie du site officiel)")
+    for (const almanax of result) {
+        const embed: MessageEmbed = await createEmbed(almanax, config.server_id);
+        message.channel.send(embed);
     }
 }
