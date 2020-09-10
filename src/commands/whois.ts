857fa731f1e31204b9e5f239a153951e32b746d1
Lethargi <paul-marie.bettinelli@epitech.eu>
2020-09-11 00:32:36 +0200

    add all player's information except ladder and characteristic link

--- src/commands/whois.ts
+++ src/commands/whois.ts
@@ -13,7 +13,7 @@ export const whois = async (message: Message, line: string[], config: any): Prom
     if (line.length < 2)
         return message.channel.send(format(sentences[config.lang].ERROR_INSUFFICIENT_ARGUMENT, `${config.prefix}whois [pseudo]`));
     line.shift();
-    const argument: string = line.join('+').toLowerCase();
+    const argument: string = line[0].toLowerCase();
     const base_url: string = `${settings.encyclopedia.base_url}/${settings.encyclopedia.player_url[config.lang]}`;
     const query_string: string = `?text=${argument}&character_level_min=1&character_level_max=200`;
     console.log(`${base_url}${query_string}`);
@@ -21,7 +21,7 @@ export const whois = async (message: Message, line: string[], config: any): Prom
     if (response.statusCode === 200) {
         try {
             const search: JSSoup = new JSSoup(response.body);
-            const search_result: any = search.findAll('tr');//.nextElement.attrs.href}`;
+            const search_result: any = search.findAll('tr');
             search_result.shift();
             let filtered_result = await search_result.filter((result: any) => {
                 return result.contents[1].nextElement.nextElement._text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
@@ -37,55 +37,60 @@ export const whois = async (message: Message, line: string[], config: any): Prom
             const answer: any = await request(link);
             if (answer.statusCode === 200) {
                 const data: any = {};
-                const soup: JSSoup = new JSSoup(answer.body);
-                const presentation: any = soup.find('div', 'ak-character-presentation');
-                const main_info: any = soup.find('div', 'ak-directories-main-infos');
-                data.name = soup.find('h1', 'ak-return-link').contents[1]._text.trim();
-                data.level = main_info.nextElement.nextElement.nextElement._text.trim();
-                data.race = main_info.contents[1].previousElement._text.trim();
-                data.title = main_info.contents[2].previousElement._text.trim();
-                data.server = server_name[config.server_id];
-                data.presentation = presentation.contents[1].nextElement._text;
-                data.presentation_date = presentation.contents[1].previousElement._text.replace(/(.*)\s/g, '');
-                data.link = link;
-                console.log(data)
-                return
-                /*const members: any = soup.findAll('div', 'ak-cell');
-                const history: any = soup.findAll('div', 'ak-title');
-                data.link = link;
-                data.guild_name = soup.find('h1').contents[1]._text.trim();
-                //data.icon = soup.find('div', 'ak-emblem').attrs.style.replace(/.*\(|\).*///g, '').replace(/[^/]*$/g, '') + "128_128-0.png";
-                //data.level = main_info.nextElement.nextElement.nextElement._text.trim().replace(/(.*)\s/g, '');
-                //data.member_number = main_info.contents[1].previousElement._text.trim().replace(/\s(.*)/g, '');
-                //data.server = soup.find('span', 'server').nextElement.nextElement.contents[0]._text.trim();
-                //data.created_at = soup.find('span', 'ak-directories-creation-date').contents[0]._text.trim().replace(/(.*)\s/g, '')
-                //data.alliance_emblem = alliance.contents[0].nextElement.attrs.style.replace(/.*\(|\).*/g, '');
-                /*data.alliance_name = alliance.contents[1].nextElement.contents[0]._text.trim();
-                data.alliance_guilds_number = alliance.contents[1].contents[2].nextElement._text.trim().replace(/\s(.*)/g, '');
-                data.alliance_members = alliance.contents[1].contents[4].nextElement._text.trim().replace(/\s(.*)/g, '');
-                data.pillars = members.map((element: any) => {
-                    return {
-                        name: element.contents[0].contents[0]._text.trim(),
-                        role: element.contents[1].contents[0]._text.trim(),
-                        lvl:  element.contents[2].contents[0]._text.trim().replace(/(.*)\s/g, '')
-                    };
-                });
-                data.activities = history.map((element: any) => {
+                try {
+                    const soup: JSSoup = new JSSoup(answer.body);
+                    const presentation: any = soup.find('div', 'ak-character-presentation');
+                    const main_info: any = soup.find('div', 'ak-directories-main-infos');
+                    const layers_info: any = soup.findAll('div', 'ak-panel-content');
+                    data.image = `${soup.find('div', 'ak-entitylook').attrs.style.replace(/.*\(|\).*/g, '').replace(/[^/]*$/g, '')}200_350-0.png`;
+                    data.name = soup.find('h1', 'ak-return-link').contents[1]._text.trim();
+                    data.level = main_info.nextElement.nextElement.nextElement._text.trim().replace(/(.*)\s/g, '');
+                    data.race = main_info.contents[1].previousElement._text.trim();
                     try {
-                        return {
-                            time: element.contents[0].contents[0]._text.replace(/(.*)il y a/g, '').replace(/:/g, '').replace(/ago/g, '').trim(),
-                            name: element.contents[1].nextElement._text.trim(),
-                            action: element.contents[1].nextElement.nextElement._text.trim()
-                        };
-                    } catch (err) {
-                        return {
-                            time: element.contents[0].contents[0]._text.replace(/(.*)il y a/g, '').replace(/:/g, '').replace(/ago/g, '').trim(),
-                            action: element.contents[0].nextElement.nextElement._text.replace(/\s*([.])/g, '').trim() + '.'
-                        };
-                    }
-                }).slice(0, 15);*/
-                console.log(data);
-                message.channel.send(await createGuildEmbed(data, config.lang));
+                        data.title = main_info.contents[2].previousElement._text.trim();
+                    } catch { };
+                    data.server = server_name[config.server_id];
+                    try {
+                        data.presentation = presentation.contents[1].nextElement._text;
+                        data.presentation_date = presentation.contents[1].previousElement._text.replace(/(.*)\s/g, '');
+                    } catch {  };
+                    try {
+                        data.guild_name = layers_info[4].contents[1].nextElement._text.trim();
+                        data.guild_link = layers_info[4].contents[1].attrs.href;
+                        data.guild_image = `${layers_info[4].contents[1].previousElement.attrs.style.replace(/.*\(|\).*/g, '').replace(/[^/]*$/g, '')}128_128-0.png`;
+                        data.guild_level = layers_info[4].contents[3].nextElement._text.trim().replace(/(.*)\s/g, '');
+                        data.guild_members = layers_info[4].contents[5].nextElement._text.trim().replace(/\s(.*)/g, '');
+                    } catch { };
+                    try {
+                        data.alliance_image = layers_info[5].contents[1].previousElement.attrs.style.replace(/.*\(|\).*/g, '');
+                        data.alliance_link = layers_info[5].contents[1].attrs.href;
+                        data.alliance_name = layers_info[5].contents[1].nextElement._text.trim();
+                        data.alliance_guilds_number = layers_info[5].contents[3].nextElement._text.trim().replace(/\s(.*)/g, '');
+                        data.alliance_members = layers_info[5].contents[4].nextElement._text.trim().replace(/\s(.*)/g, '');
+                    } catch { };
+                    try {
+                        data.success = layers_info[6].contents[0].contents[1].contents[0].nextElement.contents[0].nextElement.nextElement.nextElement._text.trim();
+                        data.success_last_time = layers_info[6].contents[1].contents[1].nextElement._text.trim().replace(/(.*)il y a/g, '').replace(/:/g, '').replace(/ago/g, '').trim();
+                        data.success_last_name = layers_info[6].contents[1].contents[3]._text.trim();
+                    } catch { };
+                    try {
+                        data.marry_name = layers_info[7].contents[2].nextElement._text.trim();
+                        data.marry_link = `${base_url}/${layers_info[7].contents[2].attrs.href}`;
+                    } catch { };
+                    try {
+                        data.jobs = layers_info[8].nextElement.contents.map((element: any) => {
+                            return {
+                                level: element.contents[0].contents[0].contents[1].contents[1].nextElement._text.trim(),
+                                name: element.contents[0].contents[0].contents[1].contents[0].nextElement.nextElement._text.trim()
+                            }
+                        });
+                    } catch { };
+                    data.link = link;
+                    console.log(data)
+                    //message.channel.send(await createGuildEmbed(data, config.lang));
+                } catch (err) {
+                    console.log(err)
+                }
             } else
                 message.channel.send(await createGuildErrorEmbed(config.lang, argument, `${base_url}${query_string}`, 0));
         } catch (err) {
