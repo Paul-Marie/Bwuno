ea1a6a51ebd99c1365f372036e2359b71535b99d
Lethargi <paul-marie.bettinelli@epitech.eu>
2021-02-25 01:56:00 +0100

    fix name and handle type

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -4,13 +4,14 @@ import { parse, HTMLElement, Node } from 'node-html-parser';
 
 const version: string = process.env.API_VERSION || "1.49.5";
 const encyclopedia: string = "https://www.dofus-touch.com/fr/mmorpg/encyclopedie/ressources";
+const imageURL: string = "https://static.ankama.com/dofus-touch/www/game/items/200";
 const suffix: string = "?game=dofustouch";
 const uri: string = "http://www.krosmoz.com/fr/almanax";
 const dates: Number[] = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 
-const getData = async () => (
+const getJSON = async (scope: string) => (
     (await axios.post(`https://proxyconnection.touch.dofus.com/data/map?lang=fr&v=${version}`, {
-        class: "Items"
+        class: scope
     }, {
         headers: {
             'origin': 'file://',
@@ -24,7 +25,7 @@ const getData = async () => (
     }) as AxiosResponse<any>).data
 );
 
-const parseData = (date: string, body: any) => {
+const parseData = (date: string, items: any[], body: any) => {
     const root: HTMLElement = parse(body);
     const event: Node[] = root.querySelector('#almanax_event_image')?.childNodes;
     const content: Node[] = root.querySelectorAll('.mid')[2].childNodes;
@@ -32,18 +33,22 @@ const parseData = (date: string, body: any) => {
     const meryde_description: Node[] = root.querySelector('#almanax_boss_desc').childNodes;
     const zodiac: Node[] = root.querySelector('.zodiac_more').childNodes;
     const offering: Node[] = root.querySelector('.more-infos-content').childNodes;
-    //const bonus: Node[] = root.querySelector('#mid').childNodes;
-    //console.dir(content[3].childNodes.map(elem => `${elem.rawText} ${elem.childNodes.map(e => ` ${e.rawText} `)}`).join(' ').replace(/\s+/g, ' ').trim(), { depth: 5 });
-    //console.dir(offering.filter((elem: any) => elem.rawAttrs !== 'class="more-infos-content"').map((elem: any) => elem.rawText.trim()).join(' ').trim().split(' ').slice(-1)[0].trim())
-    //console.dir(content[3].childNodes.filter((elem: any) => elem.rawAttrs !== 'class="more-infos"').map((elem) => `${elem.rawText}`).join(' ').replace(/\s+/g, ' ').trim(), { depth: 5 });
-    //console.dir((event) ? root.querySelector('#almanax_event_desc').childNodes : null, { depth: 5 });
-    const [ quantity, name ] = (offering.map(elem => elem.rawText).join(' ').replace(/\s+/g, ' ').trim().split(' ').filter((_, i) => i > 0 && i < 3));
+    const name: string = (offering.map(elem => elem.rawText).join(' ').replace(/\s+/g, ' ').trim().split(' ')).slice(2, -6).join(' ');
+    const quantity: number = parseInt(offering.map(elem => elem.rawText).join('').replace(/\s+/g, ' ').trim().split(' ')[1])
     //console.dir((event) ? root.querySelector('#almanax_event_desc').childNodes.map(elem => elem.rawText).join(' ').replace(/\s+/g, ' ').trim() : null, { depth: 5 });
     //console.dir((event) ? root.querySelector('#almanax_event_desc').childNodes[1].rawText.trim() : null, { depth: 3 });
     //console.dir(event) ? (root.querySelector('#almanax_event_image').childNodes[1] as any).rawAttrs.split('"')[1] : undefined),
     //console.dir(zodiac, { depth: 3 });
     //console.log(content[3].childNodes.filter((elem: any) => elem.rawAttrs !== 'class="more-infos"').map((elem) => `${elem.rawText}`).join(' ').replace(/\s+(\W)/g, "$1").trim())
-    console.log({
+    //console.dir(offering, { depth: 3 });
+    //console.log(name, quantity);
+    const category: number = items.find(item => {
+        //console.dir(offering, { depth: 3 })
+        console.log([ item.name, name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "") ]);
+        return item.name === name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+    }).type;
+    console.log(category)
+    /*console.log({
         Meryde_Name: meryde_description.filter(node => node.nodeType === 1)[0].childNodes[0].rawText.trim(),
         Meryde_Description: (meryde_description.filter(node => node.nodeType === 3)[1] as any).rawText.trim(),
         Meryde_Image: (meryde_image.filter(node => node.nodeType === 1)[0] as any).rawAttrs.split('"')[1],
@@ -58,7 +63,7 @@ const parseData = (date: string, body: any) => {
         Event_Image: (event) ? (root.querySelector('#almanax_event_image').childNodes[1] as any).rawAttrs.split('"')[1] : undefined,
         Zodiac_Name: zodiac[1].childNodes[0].rawText,
         Date: date
-    });
+    });*/
     return {
         Meryde_Name: meryde_description.filter(node => node.nodeType === 1)[0].childNodes[0].rawText.trim(),
         Meryde_Description: (meryde_description.filter(node => node.nodeType === 3)[1] as any).rawText.split('"')[1],
@@ -77,7 +82,7 @@ const parseData = (date: string, body: any) => {
     };
 }
 
-const formatData = (data: any) => {
+const getAlmanaxs = (items: any[]) => {
     const result = {};
     [...Array(12)].map((_, month) => (
         [...Array(dates[month])].map(async (_, day) => {
@@ -86,7 +91,7 @@ const formatData = (data: any) => {
             const url: string = `${uri}/${date}${suffix}`;
             const response: AxiosResponse<any> = await axios.get(url);
             if (response.status === 200) {
-                result[date] = parseData(date, response.data);
+                result[date] = parseData(date, items, response.data);
                 console.log(response.status)
             } else
                 console.error(`Invalid error code: ${response.status}`);
@@ -96,8 +101,9 @@ const formatData = (data: any) => {
 
 // Download items and almanax list and make a static JSON of data
 export default async () => {
-    const data: any = await getData();
-    const result: any = formatData(data);
-    //console.log(result);
+    const data: any = await getJSON("Items");
+    const types: any = await getJSON("ItemTypes");
+    const items: any[] = Object.values(data).map((item: any) => ({ id: item.id, name: item.nameId, type: types[item.typeId].superTypeId }));
+    const result: any = getAlmanaxs(items);
     //process.exit(0);
 };
