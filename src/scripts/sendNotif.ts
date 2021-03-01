7051c0518d3a6f86094596d99641e9189653f0e8
Lethargi <paul-marie.bettinelli@epitech.eu>
2021-03-01 02:43:54 +0100

    add sendNotification script

--- src/scripts/sendNotif.ts
+++ src/scripts/sendNotif.ts
@@ -11,21 +11,21 @@ const bot: Client = new Client();
 export default async (): Promise<void> => {
     bot.on('ready', async () => {
 		const current_date: moment.Moment = moment().tz("Europe/Paris");
-		return UserModel.find({}, async (err: any, users: mongoose.Document) => {
-			for (const id in users) {
-                const user = bot.users.cache.filter((toto: User) => toto.id === users[id].identifier);
-				users[id].subscriptions.map(async (offering) => {
-                    const date: moment.Moment = moment(offering.date, 'YYYY-MM-DD');
+		await UserModel.find({}, async (err: any, users: mongoose.Document[]) => {
+			await Promise.all(users.map(async (user: any) => {
+				const discord_user: User = await bot.users.fetch(user.identifier);
+				await Promise.all(user.subscriptions.map(async (offering) => {
+                    const date: moment.Moment = moment(offering.date, "YYYY-MM-DD");
                     const duration: moment.Duration = moment.duration(date.diff(current_date));
                     const elapsed: number = Math.round(duration.asDays());
-                    const days: number = (elapsed < 0) ? 365 + elapsed : elapsed;
-                    if (days === 7 || days === 30)
-                        await user[0].send(format(sentences[users[id].lang].SUCCESS_NOTIFICATION, days));
-                });
-            }
+                    const days: number = Math.round((elapsed < 0) ? 365 + elapsed : elapsed);
+                    if (days === 7 || days === 30) 
+						await discord_user.send(format(sentences[user.lang].SUCCESS_NOTIFICATION, offering.name, days));
+                }));
+            }));
 			bot.destroy();
 			process.exit(0);
 		});
 	});
-	bot.login(settings.discord.token);
+	await bot.login(settings.discord.token);
 };
