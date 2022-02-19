40f3751bde94206e94bc6e346dbb5e9ae0ac641d
Lethargi <paul-marie.bettinelli@epitech.eu>
2022-02-19 22:21:43 +0100

    fix `auto` command

--- src/services/auto.ts
+++ src/services/auto.ts
@@ -1,15 +1,12 @@
-import * as sentences from "../../resources/language.json";
-import Server         from "../models/server";
+import * as sentences         from "../../resources/language.json";
+import      Server            from "../models/server";
 import { CommandInteraction } from 'discord.js';
-import { format     } from 'format';
 
 // Activate or desactivate `auto_mode` for a discord' server
 export const auto = async (command: CommandInteraction, config: any): Promise<string> => {
   if (!command.memberPermissions.has(['ADMINISTRATOR']))
     return sentences[config.lang].ERROR_INSUFFICIENT_PERMISSIONS;
   if (command.options.getSubcommand() === "start") {
-    if (config.auto_mode)
-      return format(sentences[config.lang].ERROR_AUTO_ALREADY_ACTIVATED, `<#${command.channel.id}>`);
     await Server.findOneAndUpdate({ identifier: config.identifier }, { auto_mode: true, auto_channel: command.channel.id });
     return sentences[config.lang].SUCCESS_AUTO_ACTIVATED;
   } else if (command.options.getSubcommand() === "stop") {
