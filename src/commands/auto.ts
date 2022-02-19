588117fffc5f5d35437b0996920930ff0d41c8cf
Lethargi <paul-marie.bettinelli@epitech.eu>
2022-02-19 19:30:02 +0100

    add `auto` command's boiler plate

--- src/commands/auto.ts
+++ src/commands/auto.ts
@@ -1,4 +1,20 @@
 export const auto = {
-  name: "auto",
-  description: "Active ou désactive l'ajout de l'almanax du jour dans un salon à minuit"
+  name:              "auto",
+  description:       "Active ou désactive l'ajout de l'almanax du jour dans un salon à minuit",
+  options: [{
+    name:            "start",
+    description:     "Active le mode auto",
+    type:            1,
+    options: [{
+      name:          "channel",
+      description:   "Le salon dans lequels sera posté les almanax à minuit",
+      type:          7,
+      channel_types: 0,
+      required:      true
+    }]
+  }, {
+    name:            "stop",
+    description:     "Désactive le mode auto",
+    type:            1
+  }]
 };
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,18 +2,17 @@
   File to re-export all comands/* module 
 */
 export { help                   } from "./help";
-//export * as ping from "./ping";
 // export { item                   } from "./item";
 export { almanax                } from "./almanax";
 export { doziak                 } from "./doziak";
 export { type                   } from "./type";
 // export { list                   } from "./list";
-// export { auto                   } from "./auto";
+export { auto                   } from "./auto";
 export { server                 } from "./server";
 // export { lang                   } from "./lang";
 // export { prefix                 } from "./prefix";
 // export { guild                  } from "./guild";
 // export { whois                  } from "./whois";
 export { info                   } from "./info";
-// export { show                   } from "./show";
+export { show                   } from "./show";
 // export { subscribe, unsubscribe } from "./subscribe";
