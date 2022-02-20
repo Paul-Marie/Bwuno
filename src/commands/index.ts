e46a6a44961079818b3a36cace80e7cd70a61376
Lethargi <paul-marie.bettinelli@epitech.eu>
2022-02-20 22:47:08 +0100

    add `lang` command's boilerplate

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,17 +1,16 @@
 /*
   File to re-export all comands/* module 
 */
-export { help                   } from "./help";
-// export { item                   } from "./item";
-export { almanax                } from "./almanax";
-export { doziak                 } from "./doziak";
-export { type                   } from "./type";
-export { auto                   } from "./auto";
-export { server                 } from "./server";
-// export { lang                   } from "./lang";
-// export { prefix                 } from "./prefix";
-// export { guild                  } from "./guild";
-// export { whois                  } from "./whois";
-export { info                   } from "./info";
-export { show                   } from "./show";
-export { remind } from "./remind";
+export { help     } from "./help";
+// export { item  } from "./item";
+export { almanax  } from "./almanax";
+export { doziak   } from "./doziak";
+export { type     } from "./type";
+export { auto     } from "./auto";
+export { server   } from "./server";
+// export { lang  } from "./lang";
+// export { guild } from "./guild";
+// export { whois } from "./whois";
+export { info     } from "./info";
+export { show     } from "./show";
+export { remind   } from "./remind";
