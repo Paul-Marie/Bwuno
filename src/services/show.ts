0fd19d125e78b8fb7bac2b69d857352ea0a653df
Lethargi <paul-marie.bettinelli@epitech.eu>
2022-02-21 21:38:41 +0100

    make `show` command return an ephemeral response

--- src/services/show.ts
+++ src/services/show.ts
@@ -1,22 +1,22 @@
-import * as sentences         from "../../resources/language.json";
-import * as moment            from 'moment';
-import { format             } from 'format';
-import { CommandInteraction } from "discord.js";
-import User                   from "../models/user";
+import * as sentences                         from "../../resources/language.json";
+import * as moment                            from 'moment';
+import { format                             } from 'format';
+import { CommandInteraction, MessageOptions } from "discord.js";
+import User                                   from "../models/user";
 
 // Allow you to subscribe to discord' notifications
-export const show = async (command: CommandInteraction, config: any): Promise<String> => {
+export const show = async (command: CommandInteraction, config: any): Promise<String | MessageOptions> => {
   const user: any = await User.findOne({ identifier: command.user.id });
   if (user?.subscriptions?.length) {
     const list: any[] = user.subscriptions.sort((almanax1, almanax2) => (
       moment(almanax1.date, "YYYY-MM-DD", 'fr') > moment(almanax2.date, "YYYY-MM-DD", 'fr')
     ) ? 1 : -1);
     const date: Date = new Date();
-    return list.map(almanax => {
+    return { content: list.map(almanax => {
       const toto: number = moment(almanax.date).diff(moment(), 'days');
       const diff: number = (toto > 0) ? toto : moment(almanax.date).add(2, 'years').diff(moment({ y: date.getFullYear() + 1 }), 'days');
       return `**${moment(almanax.date, "YYYY-MM-DD", 'fr').format("DD MMMM")}** (${diff}): \`${almanax.name}\``
-    }).join('\n');
+    }).join('\n'), ephemeral: true } as MessageOptions;
   } else
     return format(sentences[config.lang].ERROR_NOTIFICATION_SHOW);
 }
